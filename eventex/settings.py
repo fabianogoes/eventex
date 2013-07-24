46a00bbb8d6583dbdabb114b0d86d1dc5ae2c1e1
Fabiano Góes <fabianogoes@gmail.com>
2013-07-24 03:57:15 -0300

    configurando static

--- eventex/settings.py
+++ eventex/settings.py
@@ -1,7 +1,7 @@
 # Django settings for eventex project.
 
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -16,25 +16,23 @@ PROJECT_DIR = Path(__file__).parent
 
 import dj_database_url
 
+
+#DATABASES = {
+#        'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',   # Add #'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': PROJECT_DIR.child('database.db'), # Or path to #database file if using sqlite3.
+#        # The following settings are not used with sqlite3:
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',                      # Empty for localhost #through domain sockets or '127.0.0.1' for localhost through TCP.
+#        'PORT': '',                      # Set to empty string for #default.
+#    }
+#}
+
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///' + PROJECT_DIR.child('database.db'))
 }
-
-
-'''
-DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',   # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR.child('database.db'), # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-'''
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
