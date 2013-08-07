5531f073588d4e062a84f4fcb9e70a3e7f8e299b
Fabiano <ubuntu@vm.(none)>
2013-08-07 13:57:45 -0300

    lista de inscrições pelo admin

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,13 +1,11 @@
 # coding: utf-8
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
 
     url(r'', include('eventex.core.urls', namespace='core')),
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
